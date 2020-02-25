9da924711352183d31bc14572427d1a4fb733e7d
Kremifany <34751960+Kremifany@users.noreply.github.com>
2020-02-25 10:01:28 -0500

    Authorization added and Storing Identities in Db

---
+++ Data/Migrations/20200225041135_Identity.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DutchTreat.Migrations
+{
+    public partial class Identity : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Orders",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "OrderDate",
+                value: new DateTime(2020, 2, 25, 4, 11, 34, 636, DateTimeKind.Utc).AddTicks(7314));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Orders",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "OrderDate",
+                value: new DateTime(2020, 2, 21, 17, 38, 41, 871, DateTimeKind.Utc).AddTicks(6411));
+        }
+    }
+} 
